8077aca6d38f3777cd77b0513e9f4e40c253ed7c
Jawad Itani <jawaditani@gmail.com>
2023-09-04 12:36:27 -0400

    Create location.js

---
+++ location.js
@@ -0,0 +1,11 @@
+const locationSentences = {
+    "English location sentence 1": ["Indonesian translation 1a", "Indonesian translation 1b"],
+    "English location sentence 2": ["Indonesian translation 2"],
+    // ... add more sentences for locations
+};
+
+function loadLocationSentences() {
+    // This function will be responsible for loading the sentences from the locations category
+    // and starting the exercise.
+    // We'll implement this when integrating with the main script.
+}
