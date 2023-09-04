9c4fc15ad0a47caf8ab78ae79ea1238166a66761
Jawad Itani <jawaditani@gmail.com>
2023-09-04 12:35:59 -0400

    Create clothing.js

---
+++ clothing.js
@@ -0,0 +1,11 @@
+const clothingSentences = {
+    "English clothing sentence 1": ["Indonesian translation 1a", "Indonesian translation 1b"],
+    "English clothing sentence 2": ["Indonesian translation 2"],
+    // ... add more sentences for clothing
+};
+
+function loadClothingSentences() {
+    // This function will be responsible for loading the sentences from the clothing category
+    // and starting the exercise.
+    // We'll implement this when integrating with the main script.
+}
