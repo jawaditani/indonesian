bfce2315afbbc0b4e524bb7597464f231c1859ee
Jawad Itani <jawaditani@gmail.com>
2023-09-04 19:52:43 -0400

    Update location.js

--- location.js
+++ location.js
@@ -1,11 +1,17 @@
 const locationSentences = {
-    "English location sentence 1": ["Indonesian translation 1a", "Indonesian translation 1b"],
-    "English location sentence 2": ["Indonesian translation 2"],
-    // ... add more sentences for locations
+    "This building is not a hotel": ["Gedung ini tidak hotel"],
+    "The shops are over there": ["Toko ada di sana", "Toko ada di situ"],
+    "I am over here": ["Saya ada di sini"],
+    "I am from Jakarta, not from Japan": ["Saya dari Jakarta, bukan dari Jepang", "Saya berasal dari Jakarta, bukan dari Jepang"],
+    "She returns home": ["Dia pulang"],
+    "They come and go": ["Mereka datang dan pergi"],
+    "We go to school to compete": ["Kami pergi ke sekolah untuk bersaing"],
+    "That building is a hotel and this building is an office": ["Gedung itu hotel dan gedung ini kantor"],
+    "We go home together because the beach is hot": ["Kami pulang bersama karena pantai panas"],
+  "Beach or office?": ["Pantai atau kantor"],
+  "We study at the library": ["Kami belajar di perpustakaan"],
 };
 
 function loadLocationSentences() {
-    // This function will be responsible for loading the sentences from the locations category
-    // and starting the exercise.
-    // We'll implement this when integrating with the main script.
+
 }
