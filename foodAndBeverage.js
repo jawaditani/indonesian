9ed226a27ce4a8c0712ca4ed456a3595075934fd
Jawad Itani <jawaditani@gmail.com>
2023-09-04 12:36:15 -0400

    Create foodAndBeverage.js

---
+++ foodAndBeverage.js
@@ -0,0 +1,11 @@
+const foodAndBeverageSentences = {
+    "English food sentence 1": ["Indonesian translation 1a", "Indonesian translation 1b"],
+    "English beverage sentence 2": ["Indonesian translation 2"],
+    // ... add more sentences for food and beverages
+};
+
+function loadFoodAndBeverageSentences() {
+    // This function will be responsible for loading the sentences from the food & beverage category
+    // and starting the exercise.
+    // We'll implement this when integrating with the main script.
+}
