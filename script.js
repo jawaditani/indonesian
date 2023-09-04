633edf001ae92c17251ea9e0297bce8c25688de7
Jawad Itani <jawaditani@gmail.com>
2023-09-03 23:55:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Levenshtein Distance function
 function levenshteinDistance(a, b) {
-    // ... (same as before)
+    const matrix = [];
+
+    for (let i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+
+    for (let j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    for (let i = 1; i <= b.length; i++) {
+        for (let j = 1; j <= a.length; j++) {
+            if (b.charAt(i - 1) === a.charAt(j - 1)) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1));
+            }
+        }
+    }
+
+    return matrix[b.length][a.length];
 }
 
 // Sanitize Input function
@@ -24,11 +44,21 @@ let unseenSentences = Object.keys(translations);
 let mistakes = [];
 
 translationInput.addEventListener('keyup', function(event) {
-    // ... (same as before)
+    if (event.key === 'Enter') {
+        if (checkButton.textContent === 'Check') {
+            checkTranslation();
+        } else {
+            nextSentence();
+        }
+    }
 });
 
 checkButton.addEventListener('click', function() {
-    // ... (same as before)
+    if (checkButton.textContent === 'Check') {
+        checkTranslation();
+    } else {
+        nextSentence();
+    }
 });
 
 function checkTranslation() {
@@ -51,7 +81,25 @@ function checkTranslation() {
 }
 
 function nextSentence() {
-    // ... (same as before)
+    if (unseenSentences.length > 0) {
+        const randomIndex = Math.floor(Math.random() * unseenSentences.length);
+        const randomSentence = unseenSentences[randomIndex];
+        englishText.textContent = randomSentence;
+        unseenSentences.splice(randomIndex, 1);
+        translationInput.value = '';
+        result.textContent = '';
+        checkButton.textContent = 'Check';
+    } else if (mistakes.length > 0) {
+        englishText.textContent = "Let's go through your previous mistakes";
+        unseenSentences = [...mistakes];
+        mistakes = [];
+        nextSentence();
+    } else {
+        englishText.textContent = "All done!";
+        translationInput.value = '';
+        result.textContent = '';
+        checkButton.style.display = 'none';
+    }
 }
 
 // Start with the first sentence
