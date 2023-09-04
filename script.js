800ae82759a5b95c8b0c0347f9a6ffcf354b9056
Jawad Itani <jawaditani@gmail.com>
2023-09-03 23:51:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
 // Levenshtein Distance function
 function levenshteinDistance(a, b) {
-    const matrix = [];
+    // ... (same as before)
+}
 
-    for (let i = 0; i <= b.length; i++) {
-        matrix[i] = [i];
-    }
-
-    for (let j = 0; j <= a.length; j++) {
-        matrix[0][j] = j;
-    }
-
-    for (let i = 1; i <= b.length; i++) {
-        for (let j = 1; j <= a.length; j++) {
-            if (b.charAt(i - 1) === a.charAt(j - 1)) {
-                matrix[i][j] = matrix[i - 1][j - 1];
-            } else {
-                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1));
-            }
-        }
-    }
-
-    return matrix[b.length][a.length];
+// Sanitize Input function
+function sanitizeInput(str) {
+    return str.replace(/[?,!]/g, ''); // Removes ?, , and !
 }
 
 // Rest of the script
@@ -32,35 +17,23 @@ const checkButton = document.querySelector('button');
 const translations = {
     "Hello": ["Halo"],
     "I am 10 years old": ["Umur saya sepuluh tahun", "Umurku sepuluh tahun"],
-    "I want to eat fried rice": ["Saya mau makan nasi goreng"],
-    "Hello, how are you?": ["Halo, apa kabar?"],
-    "That cat is over there": ["Kucing itu di sana"]
+    // ... add more translations
 };
 
 let unseenSentences = Object.keys(translations);
 let mistakes = [];
 
 translationInput.addEventListener('keyup', function(event) {
-    if (event.key === 'Enter') {
-        if (checkButton.textContent === 'Check') {
-            checkTranslation();
-        } else {
-            nextSentence();
-        }
-    }
+    // ... (same as before)
 });
 
 checkButton.addEventListener('click', function() {
-    if (checkButton.textContent === 'Check') {
-        checkTranslation();
-    } else {
-        nextSentence();
-    }
+    // ... (same as before)
 });
 
 function checkTranslation() {
-    const userInput = translationInput.value.trim().toLowerCase();
-    const correctTranslations = translations[englishText.textContent].map(t => t.toLowerCase());
+    const userInput = sanitizeInput(translationInput.value.trim().toLowerCase());
+    const correctTranslations = translations[englishText.textContent].map(t => sanitizeInput(t.toLowerCase()));
 
     if (correctTranslations.includes(userInput)) {
         result.textContent = "Correct!";
@@ -78,25 +51,7 @@ function checkTranslation() {
 }
 
 function nextSentence() {
-    if (unseenSentences.length > 0) {
-        const randomIndex = Math.floor(Math.random() * unseenSentences.length);
-        const randomSentence = unseenSentences[randomIndex];
-        englishText.textContent = randomSentence;
-        unseenSentences.splice(randomIndex, 1);
-        translationInput.value = '';
-        result.textContent = '';
-        checkButton.textContent = 'Check';
-    } else if (mistakes.length > 0) {
-        englishText.textContent = "Let's go through your previous mistakes";
-        unseenSentences = [...mistakes];
-        mistakes = [];
-        nextSentence();
-    } else {
-        englishText.textContent = "All done!";
-        translationInput.value = '';
-        result.textContent = '';
-        checkButton.style.display = 'none';
-    }
+    // ... (same as before)
 }
 
 // Start with the first sentence
