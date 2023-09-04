2736d0a824ac38080c671488be2de10ca1eb4665
Jawad Itani <jawaditani@gmail.com>
2023-09-03 23:23:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// Levenshtein Distance function
+function levenshteinDistance(a, b) {
+    const matrix = [];
+
+    for (let i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+
+    for (let j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    for (let i = 1; i <= b.length; i++) {
+        for (let j = 1; j <= a.length; j++) {
+            if (b.charAt(i - 1) === a.charAt(j - 1)) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1));
+            }
+        }
+    }
+
+    return matrix[b.length][a.length];
+}
+
+// Rest of the script
 const englishText = document.getElementById('englishText');
 const translationInput = document.getElementById('translationInput');
 const result = document.getElementById('result');
@@ -37,6 +63,9 @@ function checkTranslation() {
     if (correctTranslations.includes(userInput)) {
         result.textContent = "Correct!";
         result.style.color = "green";
+    } else if (correctTranslations.some(correct => levenshteinDistance(correct, userInput) === 1)) {
+        result.textContent = "Correct (typo, but we'll forgive you)";
+        result.style.color = "orange";
     } else {
         result.textContent = "Incorrect. Try again.";
         result.style.color = "red";
