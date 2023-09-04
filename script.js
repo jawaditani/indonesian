b19e0c6521cba953a48fb166bccc5c3531b05605
Jawad Itani <jawaditani@gmail.com>
2023-09-04 00:12:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,10 @@ const checkButton = document.querySelector('button');
 const translations = {
     "Hello": ["Halo"],
     "I am 10 years old": ["Umur saya sepuluh tahun", "Umurku sepuluh tahun"],
+    "I want to eat friend rice with ice tea": ["Saya mau makan nasi goreng dengan es teh"],
+    "That cat is over there": ["Kucing itu di sana"],
+    "They come and go": ["Mereka datang dan pergi"],
+    "She like chicken soup because I like orange juice": ["Dia suka sup ayam karena saya suka jus jeruk"],
     // ... add more translations
 };
 
