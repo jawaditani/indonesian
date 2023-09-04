a7919be0c351db125a3b4c41583c1f916b0ed10c
Jawad Itani <jawaditani@gmail.com>
2023-09-04 19:28:47 -0400

    Update foodAndBeverage.js

--- foodAndBeverage.js
+++ foodAndBeverage.js
@@ -2,7 +2,7 @@ const foodAndBeverageSentences = {
     "I like oranges, you like appels": ["Saya suka jeruk jeruk kamu suka apel apel", "Saya suka jeruk kamu suka apel"],
     "This cake is delicious": ["Kuenya enak", "Kue ini enak"],
     "How much is this breakfast": ["Berapa harga sarapan ini"],
-    "I bought the friend chicken from that shop": ["Saya membeli ayam goreng dari toko itu"],
+    "I bought fried chicken from that shop": ["Saya membeli ayam goreng dari toko itu"],
     "I want to ask for sweet strawberry": ["Saya mau meminta stroberi manis"],
     "Rice with salt and pepper": ["Nasi dengan garam dan merica"],
     "Chicken and meat with chili sauce": ["Ayam dan daging dengan sambal"],
