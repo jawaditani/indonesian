35dfcf4695de1b1c01accab41ca91a216cbc1d6b
Jawad Itani <jawaditani@gmail.com>
2023-09-04 12:35:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,28 @@
-// Levenshtein Distance function
-function levenshteinDistance(a, b) {
-    const matrix = [];
-
-    for (let i = 0; i <= b.length; i++) {
-        matrix[i] = [i];
-    }
-
-    for (let j = 0; j <= a.length; j++) {
-        matrix[0][j] = j;
-    }
-
-    for (let i = 1; i <= b.length; i++) {
-        for (let j = 1; j <= a.length; j++) {
-            if (b.charAt(i - 1) === a.charAt(j - 1)) {
-                matrix[i][j] = matrix[i - 1][j - 1];
-            } else {
-                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1));
-            }
-        }
-    }
-
-    return matrix[b.length][a.length];
-}
-
-// Sanitize Input function
-function sanitizeInput(str) {
-    return str.replace(/[?,!]/g, ''); // Removes ?, , and !
-}
-
-// Rest of the script
-const englishText = document.getElementById('englishText');
-const translationInput = document.getElementById('translationInput');
-const result = document.getElementById('result');
-const checkButton = document.querySelector('button');
-
-const translations = {
-    "Hello": ["Halo"],
-    "I am 10 years old": ["Umur saya sepuluh tahun", "Umurku sepuluh tahun"],
-    "I want to eat fried rice with ice tea": ["Saya mau makan nasi goreng dengan es teh"],
-    "That cat is over there": ["Kucing itu di sana"],
-    "They come and go": ["Mereka datang dan pergi"],
-    "She like chicken soup because I like orange juice": ["Dia suka sup ayam karena saya suka jus jeruk"],
-    // ... add more translations
-};
-
-let unseenSentences = Object.keys(translations);
+// Variables to hold the current set of sentences and mistakes
+let currentSentences = {};
 let mistakes = [];
+let unseenSentences = [];
+
+// Score variables
+let correctCount = 0;
+let typoCount = 0;
+let incorrectCount = 0;
+
+// Press enter key
+let canPressEnter = true;
+
+let isChecking = false; // A flag to determine if we're in the checking phase or the next sentence phase
 
 document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        if (document.getElementById('instruction').textContent === "Press 'Enter' to check") {
-            checkTranslation();
-        } else {
-            nextSentence();
-        }
-    }
-});
+    if (event.key === 'Enter' && canPressEnter) {
+        canPressEnter = false;
+        setTimeout(() => canPressEnter = true, 2000); // Re-enable after 2 seconds
 
-checkButton.addEventListener('click', function() {
-    if (translationInput.disabled) {
-        nextSentence();
-    } else {
-        checkTranslation();
+        if (isChecking) {
+            nextSentence();
+        } else {
+            checkTranslation();
+        }
     }
 });
 
@@ -72,28 +33,82 @@ function checkTranslation() {
     if (userInput === "") {
         result.textContent = "Type something first!";
         result.style.color = "red";
-        translationInput.classList.add('shake'); // Add shake effect
-        setTimeout(() => translationInput.classList.remove('shake'), 500); // Remove shake effect after 0.5 seconds
-        return; // Exit the function early
+        translationInput.classList.add('shake');
+        setTimeout(() => translationInput.classList.remove('shake'), 500);
+        return;
     }
 
-    const correctTranslations = translations[englishText.textContent].map(t => sanitizeInput(t.toLowerCase()));
+    const correctTranslations = currentSentences[englishText.textContent].map(t => sanitizeInput(t.toLowerCase()));
 
     if (correctTranslations.includes(userInput)) {
         result.textContent = "Correct!";
         result.style.color = "green";
+        correctCount++;
     } else if (correctTranslations.some(correct => levenshteinDistance(correct, userInput) === 1)) {
         result.textContent = "Correct (typo, but we'll forgive you)";
         result.style.color = "orange";
+        typoCount++;
     } else {
         result.textContent = "Incorrect. Try again.";
         result.style.color = "red";
         mistakes.push(englishText.textContent);
+        incorrectCount++;
     }
+    // Update the scoreboard
+    document.getElementById('correctCount').textContent = correctCount;
+    document.getElementById('typoCount').textContent = typoCount;
+    document.getElementById('incorrectCount').textContent = incorrectCount;
+    document.getElementById('totalCount').textContent = correctCount + typoCount + incorrectCount;
 
     translationInput.disabled = true; // Disable the input box
+    isChecking = true; // Set the flag to true, indicating we're in the checking phase
 }
 
+
+// Function to start the Verbs exercise
+function loadVerbs1() {
+    currentSentences = verbs1Sentences;
+    startExercise();
+}
+
+function loadVerbs2() {
+    currentSentences = verbs2Sentences;
+    startExercise();
+}
+
+function loadVerbs3() {
+    currentSentences = verbs3Sentences;
+    startExercise();
+}
+
+// ... Similarly, you'll have functions for other categories:
+function startLocationsExercise() {
+    currentSentences = {...locationSentences};
+    startExercise();
+}
+
+function startClothingExercise() {
+    currentSentences = {...clothingSentences};
+    startExercise();
+}
+
+function startFoodAndBeverageExercise() {
+    currentSentences = {...foodAndBeverageSentences};
+    startExercise();
+}
+
+function startQuestionsExercise() {
+    currentSentences = {...questionSentences};
+    startExercise();
+}
+
+// Function to start the exercise
+function startExercise() {
+    unseenSentences = Object.keys(currentSentences);
+    nextSentence(); // Start with the first sentence
+}
+
+// Next Sentence Function
 function nextSentence() {
     translationInput.disabled = false; // Re-enable the input box
     translationInput.focus(); // Set focus back to the input field
@@ -114,9 +129,10 @@ function nextSentence() {
         englishText.textContent = "All done!";
         translationInput.value = '';
         result.textContent = '';
-        checkButton.style.display = 'none';
     }
 }
 
-// Start with the first sentence
-nextSentence();
+// ... Rest of the script for checking translations, Levenshtein distance, etc., remains unchanged ...
+
+// You don't need to call nextSentence() at the end of this script anymore, as it will be called when a category is selected.
+
