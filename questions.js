520909faa7ffde657f982a7e281020feece8ba70
Jawad Itani <jawaditani@gmail.com>
2023-09-04 12:36:41 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,11 @@
+const questionSentences = {
+    "English question sentence 1": ["Indonesian translation 1a", "Indonesian translation 1b"],
+    "English question sentence 2": ["Indonesian translation 2"],
+    // ... add more sentences for questions
+};
+
+function loadQuestionSentences() {
+    // This function will be responsible for loading the sentences from the questions category
+    // and starting the exercise.
+    // We'll implement this when integrating with the main script.
+}
