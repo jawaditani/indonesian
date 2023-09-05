7050703efcafbcb8125093003d3d6fe5ad3bd2b3
Jawad Itani <jawaditani@gmail.com>
2023-09-04 23:34:25 -0400

    Update clothing.js

--- clothing.js
+++ clothing.js
@@ -3,9 +3,3 @@ const clothingSentences = {
     "English clothing sentence 2": ["Indonesian translation 2"],
     // ... add more sentences for clothing
 };
-
-function loadClothingSentences() {
-    // This function will be responsible for loading the sentences from the clothing category
-    // and starting the exercise.
-    // We'll implement this when integrating with the main script.
-}
